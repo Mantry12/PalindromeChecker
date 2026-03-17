cd0b2c87a2e93950fe8b79309dd0c49de29228de
Mantry12 <krishmantry4@gmail.com>
2026-03-17 22:58:24 +0530

    UC9

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,26 +1,12 @@
 import java.util.Scanner;
 
-class ListNode {
-    char data;
-    ListNode next;
-    ListNode(char data) { this.data = data; }
-}
-
 public class PalindromeChecker {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter string: ");
         String original = sc.nextLine();
 
-        // Build linked list (head insertion)
-        ListNode head = null;
-        for (char c : original.toCharArray()) {
-            ListNode newNode = new ListNode(c);
-            newNode.next = head;
-            head = newNode;
-        }
-
-        if (isPalindrome(head)) {
+        if (isPalindromeRecursive(original, 0, original.length() - 1)) {
             System.out.println("Palindrome!");
         } else {
             System.out.println("Not a palindrome.");
@@ -28,39 +14,19 @@ public class PalindromeChecker {
         sc.close();
     }
 
-    // UC8: Fast-slow pointer + reverse second half
-    static boolean isPalindrome(ListNode head) {
-        // Step 1: Find middle (fast-slow pointers)
-        ListNode slow = head, fast = head;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
+    // UC9: Recursive method
+    public static boolean isPalindromeRecursive(String str, int left, int right) {
+        // Base condition
+        if (left >= right) {
+            return true;
         }
 
-        // Step 2: Reverse second half
-        ListNode secondHalf = reverseList(slow);
-
-        // Step 3: Compare first half vs reversed second half
-        ListNode firstHalf = head;
-        while (secondHalf != null) {
-            if (firstHalf.data != secondHalf.data) {
-                return false;
-            }
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
+        // Compare first & last chars
+        if (str.charAt(left) != str.charAt(right)) {
+            return false;
         }
-        return true;
-    }
 
-    // Reverse linked list (second half)
-    static ListNode reverseList(ListNode head) {
-        ListNode prev = null, current = head;
-        while (current != null) {
-            ListNode next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
-        }
-        return prev;
+        // Recursive call
+        return isPalindromeRecursive(str, left + 1, right - 1);
     }
 }
